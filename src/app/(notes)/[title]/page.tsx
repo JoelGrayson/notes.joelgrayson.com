be4d38c4f0d247c00ff0ddf74ae3f8e84e7c0c37
Joel Grayson <joel@joelgrayson.com>
2025-05-29 17:55:40 -0700

    renamed folder to do-not-edit/notes-copied-from-obsidian

--- src/app/(notes)/[title]/page.tsx
+++ src/app/(notes)/[title]/page.tsx
@@ -4,7 +4,7 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 
 export default async function Notes({ params }: { params: { title: string } }) {
     const content=await fs.readFile(
-        path.join(process.cwd(), 'src/notes-copied-from-obsidian/do-not-edit', `${params.title}.md`),
+        path.join(process.cwd(), 'src/do-not-edit/notes-copied-from-obsidian', `${params.title}.md`),
         'utf-8'
     );
 
--- src/sync-notes.sh
+++ src/sync-notes.sh
@@ -3,7 +3,7 @@
 # ABOUT: this file copies over the notes from Obsidian
 
 obsidian_path="/Users/joelgrayson/Library/Mobile Documents/iCloud~md~obsidian/Documents/Joel's Notes/notes.joelgrayson.com"
-local_path="/Users/joelgrayson/Desktop/Software/joelgrayson.com/joelgrayson.com/notes.joelgrayson.com/src/notes-copied-from-obsidian/do-not-edit"
+local_path="/Users/joelgrayson/Desktop/Software/joelgrayson.com/joelgrayson.com/notes.joelgrayson.com/src/do-not-edit/notes-copied-from-obsidian"
 
 rm -rf "$local_path"
 cp -R "$obsidian_path" "$local_path"
