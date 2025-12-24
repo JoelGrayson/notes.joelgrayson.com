f1393c33646d8f39470b992b5738dcde57578c00
Joel Grayson <joel@joelgrayson.com>
2025-12-24 09:45:17 -0500

    upgraded to fix security vulnerabilities

--- src/app/(custom-notes)/probability/page.tsx
+++ src/app/(custom-notes)/probability/page.tsx
@@ -1,6 +1,11 @@
 import { Container, Title } from "@/app/[note]/components";
 import Content from "./Content";
 
+export const metadata = {
+    title: "Probability | Joel's Notes",
+    description: 'Includes various topics from probability (random variables, expectation), information theory (entropy), as well as logistic regression and MLE'
+};
+
 export default function Probability() {
     return <Container>
         <Title title='Probability' />
